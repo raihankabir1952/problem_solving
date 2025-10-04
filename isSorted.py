5797bca9e6eb7ab277f86ebb140c021e69681da6
raihankabir1952 <raihankabir1952@gmail.com>
2025-10-05 00:05:38 +0600

    day-1

---
+++ isSorted.py
@@ -0,0 +1,13 @@
+def isSorted(arr):
+    n = len(arr)
+    for i in range(n-1):
+        if arr[i] > arr[i+1]:
+            return False
+    return True
+
+if __name__ == "__main__":
+    arr = list(map(int, input("Enter array elements: ").split()))
+    if isSorted(arr):
+        print("Array is Sorted")
+    else:
+        print("Array is Not Sorted")
